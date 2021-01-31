b0fb3fb4a7945f92d0bb083c20873b30e08bf5cf
GGQ@SB1703 <gqguan@hotmail.com>
2021-01-31 14:28:51 +0800

    [feature] 在Trajectory()输出变量outTab中增加沿程速度和受力

--- Trajectory.m
+++ Trajectory.m
@@ -12,14 +12,14 @@ y0(5) = particle.Velocity(3);
 particles = struct;
 for i = 1:length(t)
     particles(i).Time = t(i);
-    particle.Position(1) = y(2);
-    particle.Position(2) = y(4);
-    particle.Position(3) = y(6);
+    particle.Position = [y(2),y(4),y(6)];
+    particle.Velocity = [y(1),y(3),y(5)];
     particles(i).Spec = particle;
 end
 
 % 画出轨迹
-outTab = table(t,y(:,2),y(:,6),'VariableNames',{'time','z','theta'});
+outTab = table(t,y(:,2),y(:,4),y(:,6),'VariableNames',{'time','Xz','Xr','Xtheta'});
+outTab = [outTab,table(y(:,1),y(:,3),y(:,5),'VariableNames',{'Vz','Vr','Vtheta'})];
 if max(y(:,2))>membrane.H
     % 插值计算要求查询序列为不重复的有理数（重复性要求放宽为不等于0）
     idx = (y(:,2) ~= 0 & ~isnan(y(:,2)) & ~isinf(y(:,2))); 
@@ -34,7 +34,28 @@ else
     fprintf('在考查时间内颗粒未滑出膜面！\n')
 end
 
-
+% 颗粒沿程受力变化
+FCs = arrayfun(@(x)CalcForce(operation,x,fluid,membrane),[particles.Spec],'UniformOutput',false);
+FCs = reshape(FCs,length(FCs),1); % 确保以列向量
+Fz = cellfun(@(x)x(1),FCs);
+Fr = cellfun(@(x)x(2),FCs);
+Ftheta = cellfun(@(x)x(3),FCs);
+% 列表输出
+outTab = [outTab,table(Fz,Fr,Ftheta)];
+% 绘图输出
+figure('name', '颗粒在膜面滑移时受力情况')
+subplot(3,1,1)
+plot([particles.Time],Fz,'o')
+xlabel('$t$ (s)','interpreter','latex')
+ylabel('$F_z$ (N)','interpreter','latex')
+subplot(3,1,2)
+plot([particles.Time],Fr,'o')
+xlabel('$t$ (s)','interpreter','latex')
+ylabel('$F_r$ (N)','interpreter','latex')
+subplot(3,1,3)
+plot([particles.Time],Ftheta,'o')
+xlabel('$t$ (s)','interpreter','latex')
+ylabel('$F_{\theta}$ (N)','interpreter','latex')
 end
 
 function dy = motionEq(t,y,operation,particle,fluid,membrane)
