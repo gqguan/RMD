d8264612b49d9a421026fd1ba9dba22ca04f02a0
Lou Hanwen <49742299+HWLOU@users.noreply.github.com>
2021-04-14 18:06:55 +0800

    Add files via upload

--- ParticleGrowth.m
+++ ParticleGrowth.m
@@ -1,7 +1,17 @@
-function [VG,argout] =ParticleGrowth(particle, operation)
-filename=('D:\OneDrive\BYLW\第四章\生长速率 .xlsx');
-G=xlsread(filename,'A2:G163');
-x=G(:,1);
-y=G(:,(operation.Rotation.Speed/10+2));
-VG=interp1(x,y,0.02)
+function [Vg] =ParticleGrowth(particle, operation)
+load('GD.mat')
+x=[0,10,20,30,40,50];
+y=GD(:,1);
+V=GD(:,2:7);
+%% 绘制三维分布图
+figure(1);
+[x,y]=meshgrid(x,y);
+mesh(x,y,V);
+xlabel('x');
+ylabel('y');
+%根据RPM与x得到对应生长速率
+xg=operation.Rotation.Speed;
+yg=particle.Position(1);
+Vg=interp2(x,y,V,xg,yg,'linear');
+
 end
