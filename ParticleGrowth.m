ca062156540c2a6cf71b8abdb1d75e9e9b0ba375
GGQ_TP2101 <gqguan@hotmail.com>
2021-04-17 22:58:52 +0800

    修改函数输出为颗粒体积变化

--- ParticleGrowth.m
+++ ParticleGrowth.m
@@ -1,17 +1,12 @@
-function [Vg] =ParticleGrowth(particle, operation)
-load('GD.mat')
-x=[0,10,20,30,40,50];
-y=GD(:,1);
+function [dV] = ParticleGrowth(particle, operation)
+load('GD.mat','GD') % 娄瀚文提供的颗粒生长速率数据
+x=[0,10,20,30,40,50]; 
+y=GD(:,1);                                                                                 
 V=GD(:,2:7);
-%% 绘制三维分布图
-figure(1);
-[x,y]=meshgrid(x,y);
-mesh(x,y,V);
-xlabel('x');
-ylabel('y');
 %根据RPM与x得到对应生长速率
 xg=operation.Rotation.Speed;
 yg=particle.Position(1);
-Vg=interp2(x,y,V,xg,yg,'linear');
-
+dd=interp2(x,y,V,xg,yg,'linear'); % 颗粒尺寸（等球体积的直径），单位：m/s
+% 输出颗粒体积变化率，单位：m3/s
+dV = pi/6*dd^3; 
 end
