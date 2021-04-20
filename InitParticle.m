ea4259f03eec6d396664f81af44005a25fcd44f9
GGQ_TP2101 <gqguan@hotmail.com>
2021-04-21 07:42:02 +0800

    添加主程序并删除原程序

--- InitParticle.m
+++
@@ -1,3 +0,0 @@
-function [particle,operation] = InitParticle(operation, particle)
-    operation.Rotation.AngularVelocity = operation.Rotation.Speed*2*pi/60;
-    particle.Velocity = [0,0,operation.Rotation.AngularVelocity*operation.Rotation.Radium];
