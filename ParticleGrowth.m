1e840f31756625a09cd56b83dece28418e6fcd1e
GGQ_TP2101 <gqguan@hotmail.com>
2021-06-04 16:05:36 +0800

    [feature] 假定颗粒只在膜面生长，设定颗粒位置不在膜面时的生长速率为0

--- ParticleGrowth.m
+++ ParticleGrowth.m
@@ -6,10 +6,14 @@ V = GD(:,2:7);
 %根据RPM与x得到对应生长速率
 xg = membrane.Velocity(3)/(2*pi*membrane.Radium)*60;
 yg = particle.Position(1)-membrane.Z0; % 以膜面左上点为原点转换为膜面位置
-dd = interp2(RPM,Z,V,xg,yg,'spline'); % 颗粒尺寸（等球体积的直径），单位：m/s
-if dd < 0
-%     warning('ParticleGrowth()计算得颗粒生长速率为负值，重置为零')
+if yg < min(Z) || yg > max(Z)
     dd = 0;
+else
+    dd = interp2(RPM,Z,V,xg,yg,'spline'); % 颗粒尺寸（等球体积的直径），单位：m/s
+    if dd < 0
+    %     warning('ParticleGrowth()计算得颗粒生长速率为负值，重置为零')
+        dd = 0;
+    end
 end
 % 输出颗粒体积变化率，单位：m3/s
 dV = pi/6*dd^3; 
